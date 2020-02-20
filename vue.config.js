e3db107f1e1e88c54152a576c489db5dc0b89152
zhanghaiyan <zhanghaiyan@cloudh.net.cn>
2020-02-20 11:39:57 +0800

    增加跨域访问配置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
 // vue.config.js
 const Timestamp = new Date().getTime();
-const Compressionlugin=require('compression-webpack-plugin');
+//const Compressionlugin = require("compression-webpack-plugin");
 module.exports = {
-  productionSourceMap:false,//不生成map 文件
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://ins.erdoscs.com:9999/childreninfo",
+        changeOrigin: true,
+        ws: true, // proxy websockets
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
+  },
+  productionSourceMap: false,//不生成map 文件
   lintOnSave: false, //  lint 错误是否编译警告
   // 基本路径
   publicPath: "./",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
@@ -14,17 +26,17 @@ module.exports = {
   configureWebpack: { // webpack 配置
     output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
       //publicPath: './',
-      filename: `./public/js/[name].${ process.env.VUE_APP_VERSION }.${ Timestamp }.js`,
-      chunkFilename: `./public/js/[name].${ process.env.VUE_APP_VERSION }.${ Timestamp }.js`
+      filename: `./public/js/[name].${process.env.VUE_APP_VERSION}.${Timestamp}.js`,
+      chunkFilename: `./public/js/[name].${process.env.VUE_APP_VERSION}.${Timestamp}.js`
       //filename: `[name].js`,
       //chunkFilename: `[name].js`
     },
     plugins: [
-      new Compressionlugin({
-        test:/\.js$|\.html$|\.css/, //匹配文件名
-        threshold:10240,//对超过10k 的文件压缩
-        deleteOriginalAssets: false,//是否删除原文件
-      })
+      // new Compressionlugin({
+      //   test:/\.js$|\.html$|\.css/, //匹配文件名
+      //   threshold:10240,//对超过10k 的文件压缩
+      //   deleteOriginalAssets: false,//是否删除原文件
+      // })
     ]
-  },
+  }
 };
