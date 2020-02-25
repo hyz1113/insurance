da85b5b94ea3b379a386670f4fce36364ef8cb76
zhanghaiyan <zhanghaiyan@cloudh.net.cn>
2020-02-25 17:15:31 +0800

    对电话号码输入框，增加输入事件的监听。

--- src/components/base/inputTel.vue
+++ src/components/base/inputTel.vue
@@ -3,11 +3,12 @@
     <el-link type="success">身份验证【本次内测仅针对受邀用户开放】</el-link>
     <div class="m-t-10 row-align-left">
       <el-input
+        @input="checkTel"
         size="mini"
         v-model="tel"
         placeholder="请输入受邀手机号"
       ></el-input>
-      <el-button @click="checkTel" type="primary" size="mini">验证 </el-button>
+      <!--<el-button @click="checkTel" type="primary" size="mini">验证 </el-button>-->
     </div>
   </div>
 </template>
@@ -24,17 +25,22 @@ export default {
   },
   methods: {
     checkTel() {
-
+      //debugger
+      //console.log("1111");
       if (!this.tel) {
-        this.$message("请输入受邀手机号！");
+        this.$emit('resiteIssubmitBtn');
+       // this.$message("请输入受邀手机号！");
         return;
       } else if (!isValidPhone(this.tel)) {
-        this.$message("手机号输入错误");
-        this.tel='';
+        this.$emit('resiteIssubmitBtn');
+       //this.$message("手机号输入错误");
+        //this.tel='';
         return;
+      } else{
+        this.$message("手机号验证通过！");
+        this.$emit('getTel',this.tel);
       }
-      this.$message("手机号验证通过！");
-      this.$emit('getTel',this.tel);
+
     }
   }
 };
